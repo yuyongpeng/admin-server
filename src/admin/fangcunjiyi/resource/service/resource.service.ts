52eef64be2741fe2cd51b352d0f3706c54e5a5e4
yuyongpeng <yuyongpeng@data-assets-finder.com>
2025-10-17 20:45:50 +0800

    解决prettier格式化代码的问题，针对对象会添加引号的问题

--- src/admin/fangcunjiyi/resource/service/resource.service.ts
+++ src/admin/fangcunjiyi/resource/service/resource.service.ts
@@ -1,16 +1,16 @@
-import { Injectable, Logger, Res } from '@nestjs/common';
-import { PrismaService } from '@/common/service/prisma/prisma.service';
-import { Response } from 'express';
-import { exportTable, tree } from '@/common/utils';
-import { QueryResourceDto, CreateResourceDto, UpdateResourceDto } from '../dto/index';
-import { Prisma } from '@/common/prisma-client';
-import { isNotEmpty } from 'class-validator';
-import * as assert from 'assert';
-import { HttpService } from '@nestjs/axios';
-import axios from 'axios';
-import { Config } from '@/config';
-import { MakeripException } from '@/common/exception/MakeripException';
-import { DstampError, DaopSjtHideError } from '@/common/exception/DaopException';
+import { Injectable, Logger, Res } from "@nestjs/common";
+import { PrismaService } from "@/common/service/prisma/prisma.service";
+import { Response } from "express";
+import { exportTable, tree } from "@/common/utils";
+import { QueryResourceDto, CreateResourceDto, UpdateResourceDto } from "../dto/index";
+import { Prisma } from "@/common/prisma-client";
+import { isNotEmpty } from "class-validator";
+import * as assert from "assert";
+import { HttpService } from "@nestjs/axios";
+import axios from "axios";
+import { Config } from "@/config";
+import { MakeripException } from "@/common/exception/MakeripException";
+import { DstampError, DaopSjtHideError } from "@/common/exception/DaopException";
 
 @Injectable()
 export class ResourceService {
@@ -23,34 +23,34 @@ export class ResourceService {
   /**@description 分页查询部门管理列表 */
   async selectResourceList(q: QueryResourceDto) {
     const queryCondition: Prisma.resourceWhereInput = {};
-    if (isNotEmpty(q['id'])) {
+    if (isNotEmpty(q["id"])) {
       queryCondition.id = {
         equals: q.id,
       };
     }
-    if (isNotEmpty(q['ticket_id'])) {
+    if (isNotEmpty(q["ticket_id"])) {
       queryCondition.ticket_id = {
         equals: q.ticket_id,
       };
     }
-    if (isNotEmpty(q['name'])) {
+    if (isNotEmpty(q["name"])) {
       queryCondition.name = {
         contains: q.name,
       };
     }
-    if (isNotEmpty(q['status'])) {
+    if (isNotEmpty(q["status"])) {
       queryCondition.status = {
         equals: q.status,
       };
     }
     return {
       rows: await this.prisma.resource.findMany({
-        relationLoadStrategy: 'query',
+        relationLoadStrategy: "query",
         skip: (q.pageNum - 1) * q.pageSize,
         take: q.pageSize,
         where: queryCondition,
         orderBy: {
-          create_time: 'desc',
+          create_time: "desc",
         },
         include: {
           ticket: {
@@ -123,7 +123,7 @@ export class ResourceService {
 
   /**@description 导出部门管理所有数据为xlsx */
   async exportDept(res: Response) {
-    const title = ['id', 'ticket_id', 'name', '资源URL', '排序', '创建者', '创建时间', '更新者', '更新时间', '状态（0-正常，1-删除）', '备注'];
+    const title = ["id", "ticket_id", "name", "资源URL", "排序", "创建者", "创建时间", "更新者", "更新时间", "状态（0-正常，1-删除）", "备注"];
     const data = (
       await this.prisma.resource.findMany({
         select: {
@@ -147,32 +147,13 @@ export class ResourceService {
 
   // 调用苏结通的删除NFT的接口
   async sjtHide(assetNumber: string) {
-    // let url = new URL('/sjt/hidden', Config.dstamp.baseUrl);
-    // url.searchParams.append('assetNumber', assetNumber);
-    // this.logger.log(url);
-    // this.logger.log('assetNumber=' + assetNumber);
-
     let instance = axios.create();
-    instance.defaults.headers.common['user'] = 'dstampxy';
-    instance.defaults.headers.common['pass'] = 'eyJ1c2VyX2lkIjoiZiIsIm';
+    instance.defaults.headers.common["user"] = "dstampxy";
+    instance.defaults.headers.common["pass"] = "eyJ1c2VyX2lkIjoiZiIsIm";
 
-    // let response = await instance.post(Config.dstamp.baseUrl + 'sjt/hidden', { assetNumber: assetNumber });
+    let response = await instance.post(Config.dstamp.baseUrl + "sjt/hidden", { assetNumber: assetNumber });
 
-    let response = await axios.post(
-      Config.dstamp.baseUrl + 'sjt/hidden',
-      {
-        assetNumber: assetNumber,
-      },
-      {
-        headers: {
-          'user': 'dstampxy',
-          'pass': 'eyJ1c2VyX2lkIjoiZiIsIm',
-        },
-      },
-    );
     let data = response.data;
-    this.logger.log(data);
-    data.cost;
     let success = false;
     if (data.code == 0) {
       success = true;
@@ -194,8 +175,8 @@ export class ResourceService {
   // "message":MESSAGE,
   // "code":CODE
   // }
-  async getOauthCode(client_id: string, state = '0') {
-    let url = Config.makerip.baseUrl + '/oauth/code' + '?' + 'client_id=' + client_id;
+  async getOauthCode(client_id: string, state = "0") {
+    let url = Config.makerip.baseUrl + "/oauth/code" + "?" + "client_id=" + client_id;
     let response = await axios.get(url);
     return response;
     // let code = response.code;
@@ -205,44 +186,44 @@ export class ResourceService {
     // }
   }
   async getOauthToken(client_id: string, client_secret: string, code: string) {
-    let url = Config.makerip.baseUrl + '/oauth/token' + '?' + 'client_id=' + client_id;
-    +'&client_secret=' + client_secret;
-    +'&grant_type=authorization_code';
-    +'&code=' + code;
+    let url = Config.makerip.baseUrl + "/oauth/token" + "?" + "client_id=" + client_id;
+    +"&client_secret=" + client_secret;
+    +"&grant_type=authorization_code";
+    +"&code=" + code;
     let response = await axios.get(url);
     let status = response.status;
     if (status > 0) {
-      this.logger.error('makerip 请求失败', response);
-      throw new MakeripException('makerip 请求失败');
+      this.logger.error("makerip 请求失败", response);
+      throw new MakeripException("makerip 请求失败");
     }
     return response;
   }
 
   // 刷新token
   async refreshOauthToken(client_id: string, refresh_token: string) {
-    let url = Config.makerip.baseUrl + '/oauth/refresh' + '?' + 'client_id=' + client_id;
-    +'&refresh_token=' + refresh_token;
+    let url = Config.makerip.baseUrl + "/oauth/refresh" + "?" + "client_id=" + client_id;
+    +"&refresh_token=" + refresh_token;
     let response = await axios.get(url);
     let status = response.status;
     if (status > 0) {
-      this.logger.error('makerip 请求失败', response);
-      throw new MakeripException('makerip 请求失败');
+      this.logger.error("makerip 请求失败", response);
+      throw new MakeripException("makerip 请求失败");
     }
     return response;
   }
 
   // 实名认证
   async idcardCheck(client_id: string, access_token: string, realName: string, idCard: string) {
-    let url = Config.makerip.baseUrl + '/oauth/refresh' + '?' + 'client_id=' + client_id;
-    +'&access_token=' + access_token;
-    +'&realName=' + realName;
-    +'&IDcard=' + idCard;
+    let url = Config.makerip.baseUrl + "/oauth/refresh" + "?" + "client_id=" + client_id;
+    +"&access_token=" + access_token;
+    +"&realName=" + realName;
+    +"&IDcard=" + idCard;
 
     let response = await axios.get(url);
     let status = response.status;
     if (status > 0) {
-      this.logger.error('makerip 请求失败', response);
-      throw new MakeripException('makerip 请求失败');
+      this.logger.error("makerip 请求失败", response);
+      throw new MakeripException("makerip 请求失败");
     }
     return response;
   }
